e001bed3895e9defb3154435ac6f0d6a728f1d7b
ffthylmz <ffthylmz@outlook.com>
2022-08-24 22:02:14 +0300

    Add project files.

---
+++ WFAEntity240822G/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WFAEntity240822G
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToList = new System.Windows.Forms.Button();
+            this.btnTake = new System.Windows.Forms.Button();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnContains = new System.Windows.Forms.Button();
+            this.btnWhere = new System.Windows.Forms.Button();
+            this.btnAny = new System.Windows.Forms.Button();
+            this.btnOrderBy = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnFirstOrDefault = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnGroupBy = new System.Windows.Forms.Button();
+            this.btnDataDiff = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgwList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnToList
+            // 
+            this.btnToList.Location = new System.Drawing.Point(12, 37);
+            this.btnToList.Name = "btnToList";
+            this.btnToList.Size = new System.Drawing.Size(79, 26);
+            this.btnToList.TabIndex = 0;
+            this.btnToList.Text = "ToList";
+            this.btnToList.UseVisualStyleBackColor = true;
+            this.btnToList.Click += new System.EventHandler(this.btnToList_Click);
+            // 
+            // btnTake
+            // 
+            this.btnTake.Location = new System.Drawing.Point(12, 69);
+            this.btnTake.Name = "btnTake";
+            this.btnTake.Size = new System.Drawing.Size(79, 26);
+            this.btnTake.TabIndex = 1;
+            this.btnTake.Text = "Take";
+            this.btnTake.UseVisualStyleBackColor = true;
+            this.btnTake.Click += new System.EventHandler(this.btnTake_Click);
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Location = new System.Drawing.Point(97, 69);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(79, 26);
+            this.btnSkip.TabIndex = 3;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = true;
+            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(97, 37);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(79, 26);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnContains
+            // 
+            this.btnContains.Location = new System.Drawing.Point(182, 69);
+            this.btnContains.Name = "btnContains";
+            this.btnContains.Size = new System.Drawing.Size(79, 26);
+            this.btnContains.TabIndex = 5;
+            this.btnContains.Text = "Contains";
+            this.btnContains.UseVisualStyleBackColor = true;
+            this.btnContains.Click += new System.EventHandler(this.btnContains_Click);
+            // 
+            // btnWhere
+            // 
+            this.btnWhere.Location = new System.Drawing.Point(182, 37);
+            this.btnWhere.Name = "btnWhere";
+            this.btnWhere.Size = new System.Drawing.Size(79, 26);
+            this.btnWhere.TabIndex = 4;
+            this.btnWhere.Text = "Where";
+            this.btnWhere.UseVisualStyleBackColor = true;
+            this.btnWhere.Click += new System.EventHandler(this.btnWhere_Click);
+            // 
+            // btnAny
+            // 
+            this.btnAny.Location = new System.Drawing.Point(267, 69);
+            this.btnAny.Name = "btnAny";
+            this.btnAny.Size = new System.Drawing.Size(79, 26);
+            this.btnAny.TabIndex = 7;
+            this.btnAny.Text = "Any";
+            this.btnAny.UseVisualStyleBackColor = true;
+            this.btnAny.Click += new System.EventHandler(this.btnAny_Click);
+            // 
+            // btnOrderBy
+            // 
+            this.btnOrderBy.Location = new System.Drawing.Point(267, 37);
+            this.btnOrderBy.Name = "btnOrderBy";
+            this.btnOrderBy.Size = new System.Drawing.Size(79, 26);
+            this.btnOrderBy.TabIndex = 6;
+            this.btnOrderBy.Text = "OrderBy";
+            this.btnOrderBy.UseVisualStyleBackColor = true;
+            this.btnOrderBy.Click += new System.EventHandler(this.btnOrderBy_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(607, 37);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(79, 26);
+            this.btnFind.TabIndex = 11;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnFirstOrDefault
+            // 
+            this.btnFirstOrDefault.Location = new System.Drawing.Point(522, 37);
+            this.btnFirstOrDefault.Name = "btnFirstOrDefault";
+            this.btnFirstOrDefault.Size = new System.Drawing.Size(79, 26);
+            this.btnFirstOrDefault.TabIndex = 10;
+            this.btnFirstOrDefault.Text = "FirstOrDefault";
+            this.btnFirstOrDefault.UseVisualStyleBackColor = true;
+            this.btnFirstOrDefault.Click += new System.EventHandler(this.btnFirstOrDefault_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(437, 37);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(79, 26);
+            this.btnFirst.TabIndex = 9;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnGroupBy
+            // 
+            this.btnGroupBy.Location = new System.Drawing.Point(352, 37);
+            this.btnGroupBy.Name = "btnGroupBy";
+            this.btnGroupBy.Size = new System.Drawing.Size(79, 26);
+            this.btnGroupBy.TabIndex = 8;
+            this.btnGroupBy.Text = "Group By";
+            this.btnGroupBy.UseVisualStyleBackColor = true;
+            this.btnGroupBy.Click += new System.EventHandler(this.btnGroupBy_Click);
+            // 
+            // btnDataDiff
+            // 
+            this.btnDataDiff.Location = new System.Drawing.Point(182, 134);
+            this.btnDataDiff.Name = "btnDataDiff";
+            this.btnDataDiff.Size = new System.Drawing.Size(79, 26);
+            this.btnDataDiff.TabIndex = 14;
+            this.btnDataDiff.Text = "DataDiff";
+            this.btnDataDiff.UseVisualStyleBackColor = true;
+            this.btnDataDiff.Click += new System.EventHandler(this.btnDataDiff_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(97, 134);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(79, 26);
+            this.btnSum.TabIndex = 13;
+            this.btnSum.Text = "Sum";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(12, 134);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(79, 26);
+            this.btnCount.TabIndex = 12;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Linq To Methods";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Aggregate Functions";
+            // 
+            // dgwList
+            // 
+            this.dgwList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwList.Location = new System.Drawing.Point(12, 179);
+            this.dgwList.Name = "dgwList";
+            this.dgwList.Size = new System.Drawing.Size(776, 259);
+            this.dgwList.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgwList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDataDiff);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnFirstOrDefault);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnGroupBy);
+            this.Controls.Add(this.btnAny);
+            this.Controls.Add(this.btnOrderBy);
+            this.Controls.Add(this.btnContains);
+            this.Controls.Add(this.btnWhere);
+            this.Controls.Add(this.btnSkip);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnTake);
+            this.Controls.Add(this.btnToList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToList;
+        private System.Windows.Forms.Button btnTake;
+        private System.Windows.Forms.Button btnSkip;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnContains;
+        private System.Windows.Forms.Button btnWhere;
+        private System.Windows.Forms.Button btnAny;
+        private System.Windows.Forms.Button btnOrderBy;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnFirstOrDefault;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnGroupBy;
+        private System.Windows.Forms.Button btnDataDiff;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgwList;
+    }
+}
+
---
+++ WFAEntity240822G/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.SqlServer;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAEntity240822G
+{
+    public partial class Form1 : Form
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToList_Click(object sender, EventArgs e)
+        {
+            //Categori tablosuna datagridview a yazdıralım.
+            dgwList.DataSource = db.Categories.ToList();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            //Customer CompanyName, ContactName, Phone ve Adres bilgilerini getiriniz.
+
+            dgwList.DataSource = db.Customers.Select(c => new
+            {
+                Sirket = c.CompanyName,
+                Yetkili = c.ContactName,
+                Telefon = c.Phone,
+                Adres = c.Address
+            }).ToList();
+        }
+
+        private void btnWhere_Click(object sender, EventArgs e)
+        {
+            dgwList.DataSource = db.Products.Where(a => a.UnitsInStock > 100 && a.UnitsInStock < 200).ToList();
+        }
+
+        private void btnOrderBy_Click(object sender, EventArgs e)
+        {
+            //dgwList.DataSource = db.Products.OrderBy(a => a.ProductName).ToList();
+            dgwList.DataSource = db.Products.Select(a => new
+            {
+                a.ProductID,
+                a.ProductName,
+                a.UnitsInStock,
+                a.UnitPrice
+            }).OrderByDescending(a => a.UnitsInStock).ToList();
+        }
+
+        private void btnGroupBy_Click(object sender, EventArgs e)
+        {
+            //Hangi kategoride kaç tane ürün var ?
+            dgwList.DataSource = db.Products.GroupBy(a => a.Category.CategoryName).Select(b => new
+            {
+                KategoriAdi = b.Key,
+                Toplam_Stok_Miktari = b.Sum(c => c.UnitsInStock)
+            }).ToList();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            //Kategori tablosundaki ilk veriyi geitriniz ve mesaj olarak bastırınız.
+
+            try
+            {
+                Category ct = db.Categories.Where(a => a.CategoryID > 50).First();
+                MessageBox.Show("50 ID'sine ait olan kategori " + ct.CategoryName);
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Kategori Bulunamadı.");
+            }
+
+        }
+
+        private void btnFirstOrDefault_Click(object sender, EventArgs e)
+        {
+            Category ct = db.Categories.Where(a => a.CategoryID > 50).FirstOrDefault();
+
+            if (ct == null)
+            {
+                MessageBox.Show("Aradığınız kategori bulunamadı.");
+            }
+            else
+                MessageBox.Show("İlk gelen kategorinin adı: " + ct.CategoryName);
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            //Employeelerden ıd si 1 olanı bulunuz, null dönüyorsa mesaj yazdırınız, dönmüyorsa adı ve soyadını yazdırınız.
+            Employee emp = db.Employees.Find(1);
+            if (emp != null)
+            {
+                MessageBox.Show("Adı: " + emp.FirstName + "\nSoyadı: " + emp.LastName);
+            }
+            else
+                MessageBox.Show("ID'si 1 olan çalışan bulunamadı.");
+
+        }
+
+        private void btnTake_Click(object sender, EventArgs e)
+        {
+            //product tabosundaki ürünleri UnitPrice a göre çoktn aza sıralayınız. İlk 5 ürünün ID, ProductName, UnitPrice, UnitsInStock bilgilerini getiriniz.
+            var result = db.Products.OrderByDescending(f => f.UnitPrice).Take(5).Select(x => new
+            {
+                ID = x.ProductID,
+                Name = x.ProductName,
+                Price = x.UnitPrice,
+                Stok = x.UnitsInStock
+            }).ToList();
+
+            dgwList.DataSource = result.ToList();
+        }
+
+        private void btnSkip_Click(object sender, EventArgs e)
+        {
+            //Stokta en çok bulunan ürünlerimden 10 ile 20 arasındakileri listeleyiniz.
+
+            //dgwList.DataSource = db.Products.OrderByDescending(a => a.UnitsInStock).Take(20).Skip(10).Select(a => new
+            //{
+            //    ID = a.ProductID,
+            //    Name = a.ProductName,
+            //    UnitPrice = a.UnitPrice,
+            //    Stock = a.UnitsInStock
+            //}).ToList();
+
+            dgwList.DataSource = db.Products.OrderByDescending(a => a.UnitsInStock).Select(a => new
+            {
+                ID = a.ProductID,
+                Name = a.ProductName,
+                UnitPrice = a.UnitPrice,
+                Stock = a.UnitsInStock
+            }).Take(20).Skip(10).ToList();
+        }
+
+        private void btnContains_Click(object sender, EventArgs e)
+        {
+            //Çalışanların isimleri içerisinde a harfi geçenleri listeleyelim ve 5 ile 10 arasındakileri alalım.
+
+            dgwList.DataSource = db.Employees.Where(a => a.FirstName.Contains("a")).OrderBy(z => z.FirstName).Skip(5).Take(5).ToList();
+
+        }
+
+        private void btnAny_Click(object sender, EventArgs e)
+        {
+            //Category tablosunda SeaFood diye bir kategori ismi var mı varsa sonucu yazdırın.
+
+            bool result = db.Categories.Any(c => c.CategoryName == "SeaFood");
+            if (result)
+            {
+                dgwList.DataSource = db.Categories.Where(c => c.CategoryName == "SeaFood").ToList();
+            }
+            else
+                MessageBox.Show("Kategori Mevcut Değildir.");
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            //Product tablosundaki ürün sayısını yazdırınız.
+
+            int adet = db.Products.Count();
+            MessageBox.Show("Ürün Adedimiz: " + adet);
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            //Stoğumda toplam kaç adet ürün var.
+
+            int? result = db.Products.Sum(c => c.UnitsInStock);  // int? null olan değerleri 0 kabul ederek işlem yapacağını gösteriyor.
+            MessageBox.Show("Stok Toplamı: " + result);
+        }
+
+        private void btnDataDiff_Click(object sender, EventArgs e)
+        {
+            //Çalışanların yaşlarını hesaplayınız ve isim soyisim birleşik yazarak yaşlarını yanlarına yazdırınız.
+
+            dgwList.DataSource = db.Employees.Select(x => new
+            {
+                FullName = x.FirstName + " " + x.LastName,
+                BirthDate = x.BirthDate,
+                Age = SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)
+            }).ToList();
+        }
+    }
+}
